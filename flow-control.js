54007e89722bcb3e69354952aab55bb1ff931b92
Francisco trejo <f85.trejo@gmail.com>
2021-02-22 02:25:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,6 +15,10 @@ function teenager(age) {
 function ageChecker(age) {
   if (age <= 12) {
     return "You are a kid"
+  } else if (age > 12 && age < 20) {
+    return "You are a teenager!"
+  } else {
+    return "You are a grownup"
   }
 }
 
